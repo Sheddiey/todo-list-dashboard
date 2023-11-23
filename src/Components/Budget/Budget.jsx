db9b2353ee544d96bd74695b69f97bf2354600ab
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-24 00:02:08 +0300

    completed report component

---
+++ src/Components/Budget/Budget.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './budget.css'
+
+const Budget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Budget
--- src/Components/ContentMain/ContentMain.jsx
+++ src/Components/ContentMain/ContentMain.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './contentMain.css'
 import Cards from '../Cards/Cards'
 import Transaction from '../Transaction/Transaction'
+import Report from '../Report/Report'
+import Budget from '../Budget/Budget'
 
 const ContentMain = () => {
   return (
@@ -9,6 +11,10 @@ const ContentMain = () => {
       <div className="content-grid-one">
         <Cards />
         <Transaction />
+        <Report />
+      </div>
+      <div className="content-grid-two">
+        <Budget />
       </div>
     </div>
   )
