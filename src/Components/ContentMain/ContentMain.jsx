651ff78b4641fbd0d9af473ff8af91484fd09350
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-23 22:46:30 +0300

    created transaction component

--- src/Components/ContentMain/ContentMain.jsx
+++ src/Components/ContentMain/ContentMain.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './contentMain.css'
 import Cards from '../Cards/Cards'
+import Transaction from '../Transaction/Transaction'
 
 const ContentMain = () => {
   return (
     <div className='main-content-holder'>
       <div className="content-grid-one">
         <Cards />
+        <Transaction />
       </div>
     </div>
   )
