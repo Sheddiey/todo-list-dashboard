ecd22d1d36187d42bc6c25cbacdc64d35afe68d0
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-24 19:47:26 +0300

    completed the project

--- src/Components/Financial/Financial.jsx
+++ src/Components/Financial/Financial.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import './financial.css'
+import React from "react";
+import "./financial.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Financial = () => {
   return (
-    <div>
-      
+    <div className="subgrid-two-item grid-common grid-c8">
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">Financial Advice</h3>
+        <button className="grid-c-title-icon">
+          <FontAwesomeIcon className="icon-plus" icon={faPlus} />
+        </button>
+      </div>
+      <div className="grid-c8-content">
+        <p className="text text-silver-v1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit...
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Financial
+export default Financial;
